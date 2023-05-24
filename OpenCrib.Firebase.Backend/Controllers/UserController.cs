b6a7584e749a2db2215d409f8efefc70481caa7c
Wilson Narea <wilsonnarea27@gmail.com>
2023-05-24 18:52:28 -0400

    Added public async Task<User> GetUser(string userId)
    public async Task<User> CreateUser(User user)
    
      public async Task<bool> DeleteUser(string id)
    
    Working on unfollow and follow methods
    
    Testing Token Authentication

--- OpenCrib.Firebase.Backend/Controllers/UserController.cs
+++ OpenCrib.Firebase.Backend/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Google.Cloud.Firestore;
 using OpenCrib.Firebase.Backend.Models;
 using OpenCrib.Firebase.Backend.Services;
+using Microsoft.AspNetCore.Authorization;
 
 namespace OpenCrib.Firebase.Backend.Controllers
 {
@@ -22,11 +23,43 @@ namespace OpenCrib.Firebase.Backend.Controllers
         //
         // Returns all Users in the collection
         //
-        [HttpGet]
-        [Route("GetAllUsers")]
+        
+        [HttpGet("GetAllUsers")]
         public async Task<List<User>> GetAll()
         {
+           
             return await  _firestoreDbService.GetAllUsers();
         }
+
+        [HttpGet("GetUser{userId}")]
+        public async Task<IActionResult> GetUser(string userId)
+        {
+            
+            User user = await _firestoreDbService.GetUser(userId);
+            if(user != null)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        /*[HttpPost("follow")]
+        [Authorize] // Requires authentication (token)
+        public async Task<IActionResult> FollowUser(string followeeUserId)
+        {
+            string followerUserId = User.Identity.Name; // Extract the user ID from the authenticated token
+
+            bool isFollowed = await _firestoreDbService.FollowUser(followerUserId, followeeUserId);
+
+            if (isFollowed)
+            {
+                return Ok("User followed successfully.");
+            }
+            else
+            {
+                return BadRequest("Unable to follow user.");
+            }
+        }*/
+
     }
 }
